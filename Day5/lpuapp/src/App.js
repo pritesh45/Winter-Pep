43f70d000ebb8881f291caa3261857e44426c791
Pritesh Kumar <pritesh2004kr@gmail.com>
2025-01-18 11:57:53 +0530

    commit

---
+++ Day5/lpuapp/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import './components/Header.js';
+import './components/Footer.js';
+import Header from './components/Header.js';
+import Footer  from './components/Footer.js';
+function App() {
+  return (
+    <>
+    
+    <Header></Header>
+    <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
---
+++ Day5/lpuapp/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+function Header(){
+  return (
+    <div>
+    <p>
+    My first react app
+    </p>
+    </div>
+  )
+};
+export default Header;
